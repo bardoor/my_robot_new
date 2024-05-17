309b871fe708eafd60b28c838b295eeff116fef4
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 09:53:40 +0300

    feat(model): Добавил поле

---
+++ model/field/field.py
@@ -0,0 +1,70 @@
+from ..direction import Direction
+
+
+class Field:
+    def __init__(self, environment_path: str):
+        self._cells = []
+        self._width = self._height = 0
+        self._generate(environment_path)
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    def cells(self):
+        return iter(self._cells)
+
+    def _generate(self, environment_path: str):
+        root = ET.parse(environment_path).getroot()
+        rows = root.findall("row")
+
+        self._height = len(rows)
+        self._width = len(rows[0])
+
+        for row in rows:
+            cells = row.findall("cell")
+            for cell in cells:
+                cell_walls = []
+
+                if (walls := cell.get("walls", None)) is not None:
+                    walls = walls.split(" ")
+                    for direction in walls:
+                        cell_walls.append(Direction.from_str(direction))
+
+                cell_is_painted = cell.get("painted") == "True"
+                robot = Robot() if cell.get("robot") == "True" else None
+                self._cells.append(Cell(cell_is_painted, cell_walls, robot))
+
+        self._set_neighbors()
+
+    def _set_neighbors(self):
+        cells_matrix = self._as_matrix()
+        for row in range(0, self._height):
+            for col in range(0, self._width):
+                if row > 0:
+                    cells_matrix[row][col].set_neighbor(Direction.NORTH, cells_matrix[row - 1][col])
+                if row < self._height - 1:
+                    cells_matrix[row][col].set_neighbor(Direction.SOUTH, cells_matrix[row + 1][col])
+                if col > 0:
+                    cells_matrix[row][col].set_neighbor(Direction.WEST, cells_matrix[row][col - 1])
+                if col < self._width - 1:
+                    cells_matrix[row][col].set_neighbor(Direction.EAST, cells_matrix[row][col + 1])
+
+    def _as_matrix(self):
+        matrix = []
+
+        for i in range(0, len(self._cells), self._width):
+            row = self._cells[i: i + self._width]
+            matrix.append(row)
+
+        return matrix
+
+    @property
+    def robot(self):
+        for cell in self._cells:
+            if cell.robot is not None:
+                return cell.robot
