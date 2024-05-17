ffe8f9dfc58b654d0dd488699d2984a790e32547
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 10:26:07 +0300

    feat(robot_command): Добавил класс CheckWall

--- model/robot/robot_command.py
+++ model/robot/robot_command.py
@@ -6,7 +6,7 @@ class RobotCommand(ABC):
     def __init__(self, robot=None):
         self._robot = robot
 
-    def execute(self):
+    def execute(self) -> bool:
         pass
 
     def set_robot(self, robot):
@@ -18,11 +18,11 @@ class Step(RobotCommand):
         super().__init__(robot)
         self.__direction = direction
 
-    def execute(self):
+    def execute(self) -> bool:
         if self._robot is None:
             raise Exception("Робот не задан")
 
-        self._robot.step(self.__direction)
+        return self._robot.step(self.__direction)
 
 
 class Paint(RobotCommand):
@@ -33,4 +33,16 @@ class Paint(RobotCommand):
         if self._robot is None:
             raise Exception("Робот не задан")
 
-        self._robot.paint()
+        return self._robot.paint()
+
+
+class CheckWall(RobotCommand):
+    def __init__(self, direction: Direction, robot=None):
+        super().__init__(robot)
+        self.__direction = direction
+
+    def execute(self) -> bool:
+        if self._robot is None:
+            raise Exception("Робот не задан")
+
+        return self._robot.is_wall(self.__direction)
