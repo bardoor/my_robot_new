0b0be27c279fdbbba29579615b949fcf63e11757
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 10:17:05 +0300

    fix(model): Добавил класс программы

---
+++ model/program.py
@@ -0,0 +1,14 @@
+
+
+class Program:
+    def __init__(self):
+        self.__commands = []
+
+    def add_command(self, command):
+        pass
+
+    def start_execution(self, speed: float):
+        pass
+
+
+
