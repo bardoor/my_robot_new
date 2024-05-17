6a20391e47f4d2f1270db7aedf99dd297f4a0fe0
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 15:00:12 +0300

    feat(program): Добавил вывод результатов программы

--- model/program.py
+++ model/program.py
@@ -6,23 +6,30 @@ class Program:
     __commands: [RobotCommand]
     __current: int
     __timer: threading.Timer | None
+    __results: [bool]
 
     def __init__(self):
         self.__commands = []
         self.__current = 0
         self.__timer = None
+        self.__results = []
 
     def add_command(self, command: RobotCommand):
         self.__commands.append(command)
 
-    def start_execution(self, speed: float):
-        self.__timer = threading.Timer(speed, self.__execute_current)
+    def get_results(self):
+        return self.__results
+
+    def start_execution(self, interval: float):
+        self.__timer = threading.Timer(interval, self.__execute_current)
 
     def __execute_current(self):
         if self.__current >= len(self.__commands):
             return None
 
-        self.__send_command_result(self.__commands[self.__current].execute())
+        result = self.__commands[self.__current].execute()
+        self.__results.append(result)
+        self.__send_command_result(result)
         self.__current += 1
 
     def __send_command_result(self, result):
