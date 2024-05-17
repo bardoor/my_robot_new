d9a11dca34a4e7f9ff60f13ec706df488706da13
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 11:04:59 +0300

    feat(program): Чор

--- model/program.py
+++ model/program.py
@@ -1,13 +1,19 @@
+from robot.robot_command import *
 
 
 class Program:
+    __commands: [RobotCommand]
+
     def __init__(self):
         self.__commands = []
 
-    def add_command(self, command):
-        pass
+    def add_command(self, command: RobotCommand):
+        self.__commands.append(command)
 
     def start_execution(self, speed: float):
+        pass
+
+    def __end_execution(self):
         pass
 
 
