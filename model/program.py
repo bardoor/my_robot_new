a6b21c6bf5c7e5e321a6390f512098c407503528
bardoor <dmitrybartenev58@gmail.com>
2024-05-17 11:37:38 +0300

    feat(program): Сделал исполнение команд

--- model/program.py
+++ model/program.py
@@ -1,20 +1,38 @@
+import time, threading
 from robot.robot_command import *
 
 
 class Program:
     __commands: [RobotCommand]
+    __current: int
+    __timer: threading.Timer
 
     def __init__(self):
         self.__commands = []
+        self.__current = 0
+        self.__timer = None
 
     def add_command(self, command: RobotCommand):
         self.__commands.append(command)
 
     def start_execution(self, speed: float):
+        self.__timer = threading.Timer(speed, self.__execute_current)
+
+    def __execute_current(self):
+        if self.__current >= len(self.__commands):
+            return None
+
+        self.__send_command_result(self.__commands[self.__current].execute())
+        self.__current += 1
+
+    def __send_command_result(self, result):
         pass
 
     def __end_execution(self):
-        pass
+        if self.__timer is None:
+            return None
+        self.__timer.cancel()
+
 
 
 
