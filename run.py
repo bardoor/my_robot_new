441470aa8c64b76daaf138fbd7f435abb300fbe1
samidet66 <samedit66@yandex.ru>
2024-06-27 03:16:22 +0300

    fix: исправил скрипт для запуска
    Вместо powershell теперь стандартный синтаксис bat-файлов

--- run.py
+++ run.py
@@ -1,73 +1,53 @@
-import argparse
 from pathlib import Path
-import os
 import sys
 import subprocess
 import shutil
 import logging
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--silent', action='store_true', help="silent mode: no information printed except errors.")
-parser.add_argument('-c', '--cleanup', action='store_true', help="erase venv directory when finished.")
-args = parser.parse_args()
-
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-if args.silent:
-    logging.disable(logging.CRITICAL)
 
 
-venv_path = Path('venv')
+def run() -> None:
+    '''
+    Создает виртуальную среду, устанавливает в нее необходимые зависимости
+    и запускает графическое окно робота. После чего, как только окно закрылось,
+    отключает виртуальную среду
+    '''
+    venv = Path('venv')
 
-
-def create_venv() -> None:
-    if not (venv_path.is_dir() and venv_path.exists()):
-        try:
-            logger.info("No venv found, creating...")
-            subprocess.run("py -m venv venv", check=True)
-            logger.info("Created venv")
-        except subprocess.CalledProcessError as ex:
-            logger.error(f"Couldn't create venv")
-            stderr_msg = ex.stderr.decode()
-            if stderr_msg:
-                logger.error(stderr_msg)
-            os._exit(1)
-    else:
-        logger.info("Found existing venv")
-
-
-def run_ui() -> None:
-    RUN_COMMAND = (
-        '.\\venv\\Scripts\\Activate.ps1;'
-        'pip install -r requirements.txt;'
-        'py -m robot.ipc.run_ui;'
-        'deactivate'
-    )
-    EXECUTABLE_SHELL = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
-
-    try:
-        result = subprocess.run(
-            RUN_COMMAND,
+    # Небольшая оптимизация: если папка с виртуальной средой
+    # уже имеется, то она не создается
+    if not (venv.exists() or venv.is_dir()):
+        subprocess.run(
+            f'py -m venv {venv}',
+            check=True,
             shell=True,
-            executable=EXECUTABLE_SHELL,
             stdout=sys.stdout,
             stderr=sys.stderr,
         )
-        if result.returncode != 0:
-            os._exit(1)
-    except subprocess.CalledProcessError as ex:
-        logger.error(f"Couldn't run ui")
-        stderr_msg = ex.stderr.decode()
-        if stderr_msg:
-            logger.error(stderr_msg)
+
+    command = (
+        f'.\\{venv}\\Scripts\\activate.bat && '
+        'py -m pip install -r requirements.txt && '
+        'py -m robot.ipc.run_ui && '
+        'deactivate'
+    )
     
-    if args.cleanup:
-        logger.info("Removing venv")
-        shutil.rmtree(venv_path, ignore_errors=True)
+    try:
+        subprocess.run(
+            command,
+            check=True,
+            shell=True,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+        # Удаляем папку с виртуальной средой по окончании выполнения
+        shutil.rmtree(venv, ignore_errors=True)
+    except subprocess.CalledProcessError as ex:
+        logger.error(f'Error: returned non-zero exit status {ex.returncode}')
 
 
 if __name__ == "__main__":
-    create_venv()
-    run_ui()
+    run()
